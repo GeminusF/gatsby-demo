dd606d1906fae72b78630f93370167325096b833
Fərəh Feyzullayev <ferehfeyzullayev@gmail.com>
2021-06-22 15:52:00 +0400

    Finished Gatsby Tutorial Part 3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 // Step 1: Import your component
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 // Step 2: Define your component
 const AboutPage = () => {
@@ -9,6 +10,10 @@ const AboutPage = () => {
       <p>
         Hi there! I'm the proud creator of this site, which I built with Gatsby.
       </p>
+      <StaticImage
+        src="https://www.gannett-cdn.com/media/USATODAY/USATODAY/2013/05/07/gatsby-mti-jacket-3_4.jpg"
+        alt="gatsby movie"
+      />
     </Layout>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Fərəh Feyzullayev">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        src="https://pyxis.nymag.com/v1/imgs/538/0ad/6133a245424105e22932c22ba2f5b48644-1-The-Great-Gatsby-Book.2x.rsocial.w600.jpg"
+        alt="gatsby movie"
+      />
     </Layout>
   );
 };
